be782c0b5e7cb467c23ef687fbb45b63f4ca6703
icerittikhai35 <ritthikai.khan@gmai.com>
2022-05-19 11:07:26 +0700

    Update UI App.js

--- App.js
+++ App.js
@@ -1,28 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { GameEngine } from "react-native-game-engine"
-import BestGameEver from "./index.android"
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
+// import { GameEngine } from "react-native-game-engine"
+// import BestGameEver from "./index.android"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={{display: 'flex',justifyContent : 'center',margin:'auto'}}>
-        <Text>บ่าต้นหน้าโง่</Text>
-        <Text>Ice Na Hee</Text>
-        <BestGameEver />
-        <StatusBar style="auto" />
+    <ImageBackground source={require('./assets/background2.jpg')} style={styles.image}>
+      <View style={styles.container}>
+
+        <View style={{ margin: 'auto', backgroundColor: '#dddddd', width: '50%', height: '10%', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'column' }}>
+          <Text style={{ fontSize: 22, fontWeight: 'bold' }}>Score</Text>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>85 X 10</Text>
+          <StatusBar style="auto" />
+        </View>
+
+        <View style={{ margin: 'auto', backgroundColor: '#dddddd', width: '75%', height: '25%', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'column', }}>
+          <Text style={{ fontSize: 35, fontWeight: 'bold' }}>โจทย์</Text>
+          <Text style={{ fontSize: 40, fontWeight: 'bold' }}>85 X 10</Text>
+
+          <StatusBar style="auto" />
+        </View>
+
+        <View style={{ margin: 'auto', width: '90%', height: '15%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-around' }}>
+          <TouchableOpacity
+            style={{ height: '80%', width: '30%', backgroundColor: 'gray', justifyContent: 'center', alignItems: 'center' }}
+          // onPress={onPress}
+          >
+            <View>
+              <Text style={{ fontSize: 22, fontWeight: 'bold', color: 'white' }}>คำตอบ</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{ height: '80%', width: '30%', backgroundColor: 'gray', justifyContent: 'center', alignItems: 'center' }}
+          // onPress={onPress}
+          >
+            <View>
+              <Text style={{ fontSize: 22, fontWeight: 'bold', color: 'white' }}>คำตอบ</Text>
+            </View>
+          </TouchableOpacity>
+
+
+        </View>
+
+
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop:200
+    paddingTop: 0,
+    height: '100%',
+    justifyContent: 'space-around'
   },
 });
 
